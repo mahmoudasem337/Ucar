bed6edf270fda667a1d3d844d9616a44f4b4d05c
Asem <mahmoud21-00929@student.eelu.edu.eg>
2026-01-02 19:18:44 +0300

    intial commit

---
+++ src/main/java/com/asem/ucar/UcarApplication.java
@@ -0,0 +1,13 @@
+package com.asem.ucar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asem/ucar/UcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asem.ucar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
