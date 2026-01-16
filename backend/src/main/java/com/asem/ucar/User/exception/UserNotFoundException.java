67860b98a95273ac58d18043b182a276b3c85bb9
Asem <mahmoud21-00929@student.eelu.edu.eg>
2026-01-16 22:53:40 +0300

    build(maven): Update Maven dependencies and build config
    Added dependencies for JWT, MapStruct, and mail support. Updated JUnit and Mockito versions, switched to spring-boot-starter-web, and removed unused test dependencies. Modified compiler plugin configuration for Java 21 and annotation processors, and removed unnecessary Spring Boot Maven plugin configuration.

--- backend/src/main/java/com/asem/ucar/User/exception/UserNotFoundException.java
+++ backend/src/main/java/com/asem/ucar/User/exception/UserNotFoundException.java
@@ -2,18 +2,10 @@ package com.asem.ucar.User.exception;
 
 
 public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-
-    public static UserNotFoundException byId(Long id) {
-        return new UserNotFoundException("User not found with id: " + id);
-    }
-
-    public static UserNotFoundException byEmail(String email) {
-        return new UserNotFoundException("User not found with email: " + email);
+    public UserNotFoundException() {
+        super();
     }
 }
+
 
 
