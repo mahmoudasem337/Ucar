0f474d7074b45f003aa373d8f8eeca7cddfa3d7e
Asem <mahmoud21-00929@student.eelu.edu.eg>
2026-01-16 23:08:57 +0300

    feat: Add handlers for authentication and OTP exceptions
    Added exception handlers for AuthenticationFailedException and InvalidOtpException in GlobalExceptionHandler to return appropriate HTTP status codes and error messages.

--- backend/src/main/java/com/asem/ucar/User/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/asem/ucar/User/exception/GlobalExceptionHandler.java
@@ -13,11 +13,21 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleUserNotFound(UserNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("error", "Resource not found!"));
+    }
+
+    @ExceptionHandler(AuthenticationFailedException.class)
+    public ResponseEntity<Map<String, String>> handlFailedAuth(AuthenticationFailedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(Map.of("error", ex.getMessage()));
     }
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Map<String, String>> handleIllegalArgument(IllegalArgumentException ex) {
-        return ResponseEntity.badRequest()
+
+    @ExceptionHandler(InvalidOtpException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidOtp(InvalidOtpException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
                 .body(Map.of("error", ex.getMessage()));
     }
+
+
 }
